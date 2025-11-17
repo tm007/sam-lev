0816f35822a369eabd7d279602c0b9da005ef00d
theo <89665255+tm007@users.noreply.github.com>
2025-11-17 00:11:16 -0500

    Delete samlev-cloudflare-v2 3 directory

--- samlev-cloudflare-v2 3/src/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // All requests that match files in ./samlev will be served directly as static assets.
-    // This handler only runs for non-asset routes.
-    return new Response("Not found", { status: 404 });
-  }
-};
